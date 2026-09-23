3a0296f211bcac4a817d85e90a29ab2f5ad52c6e
Diogo Ribeiro <diogo_dj@hotmail.com>
2026-09-23 18:42:58 +0100

    fix: use std black_box in Criterion example

--- examples/rust-crate/benches/add.rs
+++ examples/rust-crate/benches/add.rs
@@ -1,4 +1,5 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion};
+use std::hint::black_box;
 use rust_crate::add;
 
 fn benchmark_add(c: &mut Criterion) {
